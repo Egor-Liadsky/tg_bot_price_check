1afab1a4fbbfba84952aab5e255632c720801475
Egor Liadsky <47715067+Egor-Liadsky@users.noreply.github.com>
2022-03-11 15:38:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ HEADERS = {
     'User-Agent': 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.30',
 }
 
-bot = Bot(token='5253577953:AAFGtMiESHHJpvUXfXSFCMbbaH9_b9vbdeY')
+bot = Bot(token='5253577953:AAFsfNyxmkvxiRgCRaprROhy4CimFvY0L58')
 dp = Dispatcher(bot)
 
 logging.basicConfig(level=logging.INFO)
@@ -21,26 +21,26 @@ logging.basicConfig(level=logging.INFO)
 async def start(message: types.Message, res=False):
     await message.reply('Начало')
 
-    real_price = check()[0]
-    dt = datetime.datetime.now()
+    get_product_name = check()[1]
+    old_price = check()[0]
+
+    date: datetime.datetime = datetime.datetime.now()
+    date_format: str = date.strftime("%d/%m/%Y  %H:%M:%S")
+
     while True:
-
-        if real_price != check()[0]:
-            await message.reply(f'Наименование товара:{check()[1]}'
-                                f'\nНовая цена: {check()[0]}, старая цена: {real_price}'
-                                f'\nДата изменения: {dt.strftime("%d/%m/%Y  %H:%M:%S")}')
-
-            print(f'Наименование товара:{check()[1]}'
-                  f'\nNew price: {check()[0]}\nOld price: {real_price}'
-                  f'\nДата изменения: {dt.strftime("%d/%m/%Y  %H:%M:%S")}')
-            real_price = check()[0]
+        actual_price = check()[0]
+        if old_price != actual_price:
+            await message.reply(f'Наименование товара:{get_product_name}'
+                                f'\nНовая цена: {actual_price}, старая цена: {old_price}'
+                                f'\nДата изменения: {date_format}')
+            old_price = actual_price
         else:
             continue
 
 
 def check():
     response = requests.get(URL)
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(response.content, 'html.parser')
 
     product_name = soup.find('h1').get_text(strip=True)
     product_name = re.sub("[Ð|Ñ°·²½µ¸:³¾»º]", "", product_name)
